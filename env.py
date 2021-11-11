2a1d5bb866bfb106b84297f2c783e729f75b7f6f
Hoeun Lee <hoeunlee228@gmail.com>
2021-11-12 03:49:06 +0900

    adversarial env

--- env.py
+++ env.py
@@ -14,6 +14,9 @@ class Connect6Env():
         # state
         self.state = np.zeros((19, 19), dtype=np.int)
 
+        # reward dictionary
+        self.reward_dict = {"victory" : 10.0, "defeat" : -10.0, "step" : 0.0001, "overlap" : -0.1}
+
 
     def reset(self):
         obs = self.state
@@ -34,17 +37,17 @@ class Connect6Env():
 
         if self.state[idx] == 0.0:
             self.state[idx] = 1.0
-            reward = 0.0001
+            reward = self.reward_dict["step"]
             info = {"pass":True}
         else:
-            reward = -0.1
+            reward = self.reward_dict["overlap"]
             info = {"pass":False}
 
         next_obs = self.state
 
         if self.finish_check():
             done = True
-            reward += 10
+            reward += self.reward_dict["victory"]
         else:
             done = False
 
@@ -55,3 +58,63 @@ class Connect6Env():
         for k in self.state:
             print(*k)
 
+
+class Connect6EnvAdversarial():
+    def __init__(self) -> None:
+        # state
+        self.state = {0 : np.zeros((19, 19), dtype=np.int), 1 : np.zeros((19, 19), dtype=np.int)}
+
+        # reward dictionary
+        self.reward_dict = {"victory" : 10.0, "defeat" : -10.0, "step" : 0.0001, "overlap" : -0.1}
+
+
+    def reset(self):
+        obs = self.state[0]
+        return obs
+
+    
+    def finish_check(self, turn : int) -> bool:
+        if (turn == 0):
+            pass
+
+        else:
+            pass
+
+
+    def step(self, action : int, turn : int) -> tuple(np.ndarray, float, bool, dict):
+        idx = (action // 19, action % 19)
+
+        if (turn == 0):
+            if self.state[0][idx] == 0.0:
+                self.state[0][idx] = 1.0
+                self.state[1][idx] = -1.0
+                reward = self.reward_dict["step"]
+                info = {"pass":True}
+            else:
+                reward = self.reward_dict["overlap"]
+                info = {"pass":False}
+
+        else:
+            if self.state[1][idx] == 0.0:
+                self.state[1][idx] = 1.0
+                self.state[0][idx] = -1.0
+                reward = self.reward_dict["step"]
+                info = {"pass":True}
+            else:
+                reward = self.reward_dict["overlap"]
+                info = {"pass":False}
+
+        next_obs = self.state[turn]
+
+        if self.finish_check(turn):
+            done = True
+            reward += self.reward_dict["victory"]
+        else:
+            done = False
+
+        return next_obs, reward, done, info
+
+
+    def update(self):
+        for k in self.state:
+            print(*k)
